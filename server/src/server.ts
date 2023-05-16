17acfe883562210d3ba7d65e05a5d3d55f604182
Davi Alves <cadwvk@gmail.com>
2023-05-16 17:05:50 -0300

    Added eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,15 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 const app = fastify()
 
-app.get("/hello", () => {
-  return "Hello world"
+app.get('/hello', () => {
+  return 'Hello world'
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log("Servidor rodando em http://localhost:3333 🚀")
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Servidor rodando em http://localhost:3333 🚀')
+  })
