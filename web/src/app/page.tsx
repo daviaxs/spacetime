e2fec9369b3ddcf64343585f8b33c0b701b9e58f
Davi Alves <cadwvk@gmail.com>
2023-05-17 15:55:28 -0300

    Added fonts

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1 className="text-xl font-bold text-slate-800">Hello world</h1>
+  return (
+    <div className="flex h-screen flex-col justify-center bg-zinc-950 p-6 text-white">
+      <h1 className="text-4xl font-extrabold">Font roboto</h1>
+      <h1 className="font-var text-4xl font-bold">Font bai jamjuree</h1>
+    </div>
+  )
 }
