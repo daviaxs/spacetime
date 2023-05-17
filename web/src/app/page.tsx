c7caf9432c88c0877c21c2eeee04bca3f5720996
Davi Alves <cadwvk@gmail.com>
2023-05-17 17:44:54 -0300

    Added copyright* on home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -51,8 +51,8 @@ export default function Home() {
           Feito com 💜 no NLW da{' '}
           <a
             href="https://rocketseat.com.br"
-            target="_blank"
             className="underline hover:text-gray-100"
+            target="_blank"
             rel="noreferrer"
           >
             Rocketseat
