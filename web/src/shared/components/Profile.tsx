932c43481caba87486437c0688636901bdc94c7d
Davi Alves <cadwvk@gmail.com>
2023-05-18 16:22:51 -0300

    Updated profile component

--- web/src/shared/components/Profile.tsx
+++ web/src/shared/components/Profile.tsx
@@ -1,19 +1,25 @@
-import { User } from 'lucide-react'
+import { getUser } from '@/lib/auth'
+import Image from 'next/image'
 
 export function Profile() {
-  return (
-    <a
-      href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-      className="flex items-center gap-3 text-left"
-      rel="noreferrer"
-    >
-      <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-        <User className="h-5 w-5 text-gray-500" />
-      </div>
+  const { name, avatarUrl } = getUser()
 
-      <p className="max-w-[140px] text-sm leading-snug transition-colors hover:text-gray-50">
-        <span className="underline">Crie sua conta</span> e salve suas memórias!
+  return (
+    <div className="flex items-center gap-3 text-left">
+      <Image
+        src={avatarUrl}
+        width={40}
+        height={40}
+        alt=""
+        className="h-10 w-10 rounded-full"
+      />
+
+      <p className="max-w-[140px] text-sm leading-snug">
+        {name}
+        <a href="" className="block text-red-400 hover:text-red-300">
+          Quero sair
+        </a>
       </p>
-    </a>
+    </div>
   )
 }
