bd1de44c789db31db91a7ad48807c27aa175abd3
Davi Alves <cadwvk@gmail.com>
2023-05-18 14:38:19 -0300

    Added jwt

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import 'dotenv/config'
 import fastify from 'fastify'
 import { authRoutes } from './routes/auth'
@@ -8,6 +9,10 @@ const app = fastify()
 
 app.register(cors, {
   origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
 })
 
 app.register(authRoutes)
