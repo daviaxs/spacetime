afcae654e8549bdb255bc9edbec7d98ea63aba35
Davi Alves <cadwvk@gmail.com>
2023-05-18 13:38:03 -0300

    EmptyMemories added in component file

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Copyright } from '@/shared/components/Copyright'
+import { EmptyMemories } from '@/shared/components/EmptyMemories'
 import { Hero } from '@/shared/components/Hero'
 import { SignIn } from '@/shared/components/SignIn'
 
@@ -20,17 +21,7 @@ export default function Home() {
 
       {/* Right */}
       <div className="flex flex-col bg-[url(../shared/assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a
-              href=""
-              className="underline transition-colors hover:text-gray-50"
-            >
-              criar agora!
-            </a>
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   )
