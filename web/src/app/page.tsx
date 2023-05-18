9b24ba07304736f6a6aa1680f9122abd5c0d1de8
Davi Alves <cadwvk@gmail.com>
2023-05-18 13:02:19 -0300

    SignIn and Hero added in component file

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,62 +1,25 @@
-import { User } from 'lucide-react'
-import Image from 'next/image'
-
 import { Copyright } from '@/shared/components/Copyright'
-import SpacetimeLogo from '../shared/assets/nlw-spacetime-logo.svg'
+import { Hero } from '@/shared/components/Hero'
+import { SignIn } from '@/shared/components/SignIn'
 
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
-      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../shared/assets/bg-stars.svg)] bg-cover px-28 py-16">
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
 
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        {/* Sign in */}
-        <a
-          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-          className="flex items-center gap-3 text-left"
-          rel="noreferrer"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
+        <SignIn />
 
-          <p className="max-w-[140px] text-sm leading-snug transition-colors hover:text-gray-50">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </a>
+        <Hero />
 
-        {/* Hero */}
-        <div className="mt-11 space-y-5">
-          <Image src={SpacetimeLogo} alt="Logo spacetime" />
-
-          <div className="max-w-[420px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-
-          <a
-            href=""
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-green-600"
-          >
-            CADASTRAR LEMBRANÇA
-          </a>
-        </div>
-
-        {/* Copyright */}
         <Copyright />
       </div>
 
       {/* Right */}
-      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+      <div className="flex flex-col bg-[url(../shared/assets/bg-stars.svg)] bg-cover p-16">
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
             Você ainda não registrou nenhuma lembrança, comece a{' '}
