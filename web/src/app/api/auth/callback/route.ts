6298e6bcfb9fbcb89416c0aa73c920fbd4465b5c
Davi Alves <cadwvk@gmail.com>
2023-05-18 15:38:08 -0300

    Added axios on web

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -2,6 +2,5 @@ import { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
-
-  console.log(searchParams)
+  const code = searchParams.get('code')
 }
