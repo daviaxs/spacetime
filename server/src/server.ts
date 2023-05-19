681e841386a323bc93d34d18f64453cd8e790d50
Davi Alves <cadwvk@gmail.com>
2023-05-19 19:43:32 -0300

    Added fastify static

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import jwt from '@fastify/jwt'
 import multipart from '@fastify/multipart'
 import 'dotenv/config'
 import fastify from 'fastify'
+import { resolve } from 'node:path'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 import { uploadRoutes } from './routes/upload'
@@ -10,6 +11,11 @@ import { uploadRoutes } from './routes/upload'
 const app = fastify()
 
 app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
