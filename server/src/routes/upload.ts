6001410b292d2778e9fc82a68861c8f551cd213f
Davi Alves <cadwvk@gmail.com>
2023-05-19 19:17:36 -0300

    created valid files mimetype regex

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -11,5 +11,12 @@ export async function uploadRoutes(app: FastifyInstance) {
     if (!upload) {
       return reply.status(400).send()
     }
+
+    const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
+    const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
+
+    if (!isValidFileFormat) {
+      return reply.status(400).send()
+    }
   })
 }
