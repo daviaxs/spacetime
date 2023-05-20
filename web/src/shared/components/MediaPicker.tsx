039e80120fe4501e2082c7d3be6de2286b3cc9cf
Davi Alves <cadwvk@gmail.com>
2023-05-20 14:25:25 -0300

    Updated media picker component

--- web/src/shared/components/MediaPicker.tsx
+++ web/src/shared/components/MediaPicker.tsx
@@ -1,18 +1,31 @@
 'use client'
 
-import { ChangeEvent } from 'react'
+import Image from 'next/image'
+import { ChangeEvent, useState } from 'react'
 
 export function MediaPicker() {
+  const [preview, setPreview] = useState<string | null>(null)
+
   function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
     console.log(event.target.files)
   }
 
   return (
-    <input
-      onChange={onFileSelected}
-      type="file"
-      id="media"
-      className="invisible h-0 w-0"
-    />
+    <div>
+      <input
+        onChange={onFileSelected}
+        type="file"
+        id="media"
+        className="invisible h-0 w-0"
+      />
+
+      {preview && (
+        <Image
+          src={preview}
+          alt=""
+          className="aspect-video w-full rounded-lg object-cover"
+        />
+      )}
+    </div>
   )
 }
