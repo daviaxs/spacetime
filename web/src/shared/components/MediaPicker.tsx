3c0dc6e5fcba9277288857dcc766134fb6295e47
Davi Alves <cadwvk@gmail.com>
2023-05-20 14:41:54 -0300

    Created onFileSelected func

--- web/src/shared/components/MediaPicker.tsx
+++ web/src/shared/components/MediaPicker.tsx
@@ -1,31 +1,41 @@
 'use client'
 
-import Image from 'next/image'
-import { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent } from 'react'
+const { useState } = React
 
 export function MediaPicker() {
   const [preview, setPreview] = useState<string | null>(null)
 
   function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
-    console.log(event.target.files)
+    const { files } = event.target
+
+    if (!files) {
+      return
+    }
+
+    const previewUrl = URL.createObjectURL(files[0])
+
+    setPreview(previewUrl)
   }
 
   return (
-    <div>
+    <>
       <input
         onChange={onFileSelected}
         type="file"
         id="media"
+        accept="image/*"
         className="invisible h-0 w-0"
       />
 
       {preview && (
-        <Image
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
           src={preview}
           alt=""
           className="aspect-video w-full rounded-lg object-cover"
         />
       )}
-    </div>
+    </>
   )
 }
