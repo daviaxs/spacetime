1ca26694489e1b18b826f4dce4151b9b0412b4c8
Davi Alves <cadwvk@gmail.com>
2023-05-24 16:39:57 -0300

    Added onClick in props IconButton component

--- web/src/shared/components/IconButton.tsx
+++ web/src/shared/components/IconButton.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-export function IconButton({ children }: { children: React.ReactNode }) {
+interface IIconButtonProps {
+  children: React.ReactNode
+  onClick: () => void
+}
+
+export function IconButton({ children, onClick }: IIconButtonProps) {
   return (
-    <button className="flex h-7 w-7 items-center justify-center rounded border-[0.5px] border-gray-400 bg-gray-600">
+    <button
+      onClick={onClick}
+      className="flex h-7 w-7 items-center justify-center rounded border-[0.5px] border-gray-400 bg-gray-600"
+    >
       {children}
     </button>
   )
